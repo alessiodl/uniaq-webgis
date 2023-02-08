39b10b9a36659a3ca4e65861ec0f20a0c9bb0b7c
Alessio Di Lorenzo <alessio.dl@gmail.com>
2023-02-08 15:43:51 +0100

    Update map.js

--- src/app/js/map.js
+++ src/app/js/map.js
@@ -52,10 +52,10 @@ var satellite = L.layerGroup([
 ]).addTo(map);
 */
 var mappa_base = L.layerGroup([
-	L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+	/*L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 		subdomains: 'abcd'
-	}), 
+	}), */
 	L.tileLayer('http://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png',{
 		maxZoom: 18,
         attribution: '&copy; OpenStreetMap, &copy; CartoDB',
